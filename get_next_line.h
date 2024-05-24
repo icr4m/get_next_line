5b1e512e2b62ae258820941f89734a6986625204
Icham Jaber <ijaber@student.42.fr>
2024-05-24 14:28:52 +0200

    update

--- get_next_line.h
+++ get_next_line.h
@@ -2,11 +2,11 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+        
+/*                                                    +:+ +:+
 	+:+     */
-/*   By: ijaber <ijaber@student.42.fr>              +#+  +:+      
+/*   By: ijaber <ijaber@student.42.fr>              +#+  +:+
 	+#+        */
-/*                                                +#+#+#+#+#+  
+/*                                                +#+#+#+#+#+
 	+#+           */
 /*   Created: 2024/05/18 10:24:41 by ijaber            #+#    #+#             */
 /*   Updated: 2024/05/18 10:24:41 by ijaber           ###   ########.fr       */
@@ -25,15 +25,15 @@
 #  define BUFFER_SIZE 300000
 # endif
 
-char	*get_next_line(int fd);
-char	*fill_line(int fd, char *buffer, char *left);
-char	*set_line(char *line);
-char	*ft_substr(const char *s, unsigned int start, size_t len);
+char			*get_next_line(int fd);
+char			*fill_line(int fd, char *buffer, char *left);
+char			*set_line(char *line);
+char			*ft_substr(const char *s, unsigned int start, size_t len);
 unsigned int	ft_strlcpy(char *dest, char *src, size_t size);
-size_t	ft_strlen(const char *str);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
+size_t			ft_strlen(const char *str);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+char			*ft_strdup(const char *s1);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strchr(const char *s, int c);
 
 #endif
